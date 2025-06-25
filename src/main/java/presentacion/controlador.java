6f34f57262a9a7b48a1bcc02a77bc91ffa6b3d96
ERUWIEL <angelerubielflores@gmail.com>
2025-06-25 01:45:12 -0600

    gui bloqueo

--- src/main/java/presentacion/controlador.java
+++ src/main/java/presentacion/controlador.java
@@ -1,18 +1,42 @@
-
 package presentacion;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import presentacion.vistas.pnlBloqueo;
 
 /**
  *
  * @author erwbyel
  */
-public class controlador {
+public class Controlador extends JFrame {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("HOLA PAPU");
+        SwingUtilities.invokeLater(() -> {
+            Controlador control = new Controlador();
+            mostrarBloqueo(control);
+            control.setVisible(true);
+        });
     }
-    
+    /**
+     * metodo que instancia la ventana principal
+     */
+    public Controlador() {
+        setTitle("bloqueo");
+        setSize(1440, 1050);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    /**
+     * metodo de control
+     * @param frame 
+     */
+    public static void mostrarBloqueo(JFrame frame) {
+        frame.setContentPane(new pnlBloqueo());
+        frame.revalidate();
+    }
 }
